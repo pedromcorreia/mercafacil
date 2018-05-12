fe574f3c7c62947f89660ca728afeea5a2c1b958
pedmcor <pedmcor@gmail.com>
2018-05-12 20:29:26 -0300

    Add the mappings for item and change local for index

--- lib/miner/parser.ex
+++ lib/miner/parser.ex
@@ -3,16 +3,30 @@ defmodule Miner.Parser do
   import CSV
   @chunk_size 20
   @mappings_sale %{
-    id_loja: %{type: :integer, local: 0},
-    id_venda: %{type: :integer, local: 1},
-    numero_caixa: %{type: :integer, local: 2},
-    data_venda: %{type: :date, local: 3},
-    hora_venda: %{type: :time, local: 4},
-    valor_total_sem_desc: %{type: :integer, local: 5},
-    valor_desconto: %{type: :integer, local: 6},
-    valor_total_com_desc: %{type: :integer, local: 7},
-    id_cliente_1: %{type: :integer, local: 8},
-    id_cliente_2: %{type: :integer, local: 9}
+    id_loja: %{type: :integer, index: 0},
+    id_venda: %{type: :integer, index: 1},
+    numero_caixa: %{type: :integer, index: 2},
+    data_venda: %{type: :date, index: 3},
+    hora_venda: %{type: :time, index: 4},
+    valor_total_sem_desc: %{type: :integer, index: 5},
+    valor_desconto: %{type: :integer, index: 6},
+    valor_total_com_desc: %{type: :integer, index: 7},
+    id_cliente_1: %{type: :integer, index: 8},
+    id_cliente_2: %{type: :integer, index: 9}
+  }
+  @mapping_sale_intem %{
+    id_loja: %{type: :integer, index: 0},
+    id_venda: %{type: :integer , index: 1},
+    numero_caixa: %{type: :integer, index: 2},
+    id_produto: %{type: :integer, index: 3},
+    quantidade: %{type: :decimal,index: 4},
+    valor_unitario: %{type: :decimal, index: 5},
+    valor_total_sem_desc: %{type: :decimal, index: 6},
+    valor_desconto: %{type: :decimal, index: 7},
+    valor_total_com_desc: %{type: :decimal, index: 8},
+    valor_total_sem_desc: %{type: :decimal, index: 9},
+    id_profissional_1: %{type: :integer, index: 10},
+    id_profissional_2: %{type: :integer, index: 11},
   }
 
   def parse(path) do
@@ -28,17 +42,16 @@ defmodule Miner.Parser do
   def create_entities(chunk) do
     chunk
     |> Enum.map(fn(x) ->
-      x =
-        x
-        |> List.first
-        |> String.split(";")
+      x
+      |> List.first
+      |> String.split(";")
     end)
   end
 
   def read_map(sale) do
     @mappings_sale
-    |> Enum.map(fn({field, %{local: local, type: type}}) ->
-      read_field(Enum.at(sale, local), field, type)
+    |> Enum.map(fn({field, %{index: index, type: type}}) ->
+      read_field(Enum.at(sale, index), field, type)
     end)
   end
 
